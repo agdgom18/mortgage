ac9b010e63ec62e147ede8734bcf97c97401a13d
agdgom18 <agdgomelashvili18@gmail.com>
2023-05-02 11:49:38 +0400

    feat: add while loops

--- main.java
+++ main.java
@@ -7,21 +7,46 @@ import java.util.Scanner;
 public class MortgagePayments {
     public static void main(String [] args) {
 
-        System.out.print("Principal: ");
+        // Principal
         Scanner scanner = new Scanner(System.in);
-        int principal = scanner.nextInt();
+        int principal = 0;
+        while (true){
+            System.out.print("Principal($1K - $1M): ");
+            principal = scanner.nextInt();
+            if (principal>= 1000 && principal <= 1_000_000){
+                break;
+            }
+            System.out.println("Enter number between  1k and 1m!");
+        }
 
         //rate
-        System.out.print("Annual Interest Rate: ");
         Scanner scanner1 = new Scanner(System.in);
-        float rate = (float) (scanner1.nextFloat()) / 100 /12;
+        float rate = 0;
+        while (true) {
+            System.out.print("Annual Interest Rate: ");
+            rate = scanner1.nextFloat();
+            if (rate > 1 && rate <= 30) {
+                rate = rate / 100 / 12;
+                break;
+            }
+            System.out.println("Enter rate from 2 to 30 percentage!");
+        }
         //Period
-        System.out.print("Period(Month): " );
         Scanner scanner2 = new Scanner(System.in);
-        short period = scanner2.nextShort();
+        short period = 0;
+
+        while (true) {
+            System.out.print("Period(Month): ");
+            period = scanner2.nextShort();
+            if (period > 1 &&  period < 360){
+                break;
+            }
+            System.out.println("Enter period from 2 to 360 months!");
+        }
         //Mortgage
         double result = rate*(Math.pow(1+rate,period)) / (Math.pow(1+rate,period)-1) * principal;
          String m = NumberFormat.getCurrencyInstance().format(result);
         System.out.println("Mortgage:" +m);
 
     }
+}
